5122d765a951093616ab3722d726e8a153f13677
majedabdalla <125223672+majedabdalla@users.noreply.github.com>
2025-10-10 00:11:54 +0300

    Update db.py

--- db.py
+++ db.py
@@ -24,7 +24,6 @@ async def insert_room(room_data):
     await db.rooms.insert_one(room_data)
 
 async def delete_room(room_id):
-    # Completely deletes the room document from MongoDB
     await db.rooms.delete_one({"room_id": room_id})
 
 async def insert_report(report_data):
